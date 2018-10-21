ea23a413fa3610024a1a31eb55366d0a7943c063
“chenrui <792608290@qq.com>
2018-10-22 02:35:39 +0800

    add at 10:35 2018/10/22 at lab

--- svm_pso_predictor_mTVAC-test.py
+++ svm_pso_predictor_mTVAC-test.py
@@ -7,20 +7,20 @@ from sklearn import svm
 from sklearn.model_selection import train_test_split
 import cmath
 
-# data = pd.read_csv('data/drug_cell/drug/AEW541_train_data-rfe.csv')
-# X = data.iloc[:, :-1]
-# y = data.iloc[:, -1]
-# x_train, x_test, y_train, y_test = train_test_split(X, y, random_state=1, train_size=0.7)
+data = pd.read_csv('data/drug_cell/drug/Paclitaxel_train_data-rfe.csv')
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1]
+x_train, x_test, y_train, y_test = train_test_split(X, y, random_state=1, train_size=0.7)
 
 MAX_ITER = 500
 
-data = pd.read_csv('data/drug_cell/drug/Lapatinib_train_data-rfe.csv')
-data_test = pd.read_csv('data/CGP/drug_cell/common_drugs/Lapatinib_train_data.csv')
-x_train = data.iloc[:, :-1]
-y_train = data.iloc[:, -1]
-
-x_test = data_test.iloc[:, :-1]
-y_test = data_test.iloc[:, -1]
+# data = pd.read_csv('data/drug_cell/drug/Lapatinib_train_data-rfe.csv')
+# data_test = pd.read_csv('data/CGP/drug_cell/common_drugs/Lapatinib_train_data.csv')
+# x_train = data.iloc[:, :-1]
+# y_train = data.iloc[:, -1]
+#
+# x_test = data_test.iloc[:, :-1]
+# y_test = data_test.iloc[:, -1]
 
 
 # ----------------------PSO参数设置---------------------------------
